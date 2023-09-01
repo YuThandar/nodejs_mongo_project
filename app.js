bf425c3913e4fe5f02248ef4fa60c6f6584d1557
Yu Thandar <yuthandar@yig.local>
2023-09-01 16:40:08 +0630

    Initalize the project

---
+++ app.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//   //   res.status(200).send('Hello from the server side!');
+//   res.status(200).json({
+//     message: 'Hello from the server side!',
+//     app: 'Natours',
+//   });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endppoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'status',
+    results: tours.length,
+    data: {
+      // tours: tours(read file result 'tours')
+      tours,
+    },
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
